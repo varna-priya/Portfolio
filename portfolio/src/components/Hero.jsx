911aa1161941bf55d86bb9deaadd597fac7cb559
varna-priya <varnapriya2659@gmail.com>
2026-09-22 19:20:51 +0530

    clean structure

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="min-h-[85vh] flex items-center justify-center pt-24 pb-12 px-6">
+    <section id="about" className="min-h-[85vh] flex items-center justify-center pt-24 pb-12 px-6">
       <div className="max-w-6xl w-full grid grid-cols-1 lg:grid-cols-12 gap-12 items-center">
         {/* Left Column: Text & Intro */}
         <div className="lg:col-span-7 space-y-6 text-left">
